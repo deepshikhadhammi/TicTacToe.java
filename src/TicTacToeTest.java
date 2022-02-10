f57527094bd6820a98d38c1d20f621e826c59e9e
deepshikha.dhammi <deepshikhadhammi@ucalgary.ca>
2022-02-09 21:01:31 -0700

    Testing of rowsln function that finds the dimensions of the row in a board.

--- src/TicTacToeTest.java
+++ src/TicTacToeTest.java
@@ -49,4 +49,48 @@ class TicTacToeTest {
         int[][] result = TicTacToe.createBoard(r, c);
         assertArrayEquals(exp_res, result, "Board is not valid");
     }
+
+    @Test
+    void row_length_t1()
+    {   int [][]b=new int[][]{{X,O,O},{O,X,O},{O,X,X}};
+        int exp_res=3;
+        int result=TicTacToe.rowsln(b);
+        assertEquals(exp_res,result,"Row length should is not 3");
+    }
+    @Test
+    void row_length_t2()
+    {
+        int [][]b=new int[][]{{X,O,O,X,O},{O,X,O,X,X},{O,X,X,O,O},{O,X,X,O,X}};
+        int exp_res=4;
+        int result=TicTacToe.rowsln(b);
+        assertEquals(exp_res,result,"Row length should is not 4");
+
+    }
+    @Test
+    void row_length_t3()
+    {
+        int [][]b=new int[][]{{X,X,O,O},{X,O,O,X},{O,O,X,O},{O,X,O,X},{X,X,O,O}};
+        int exp_res=5;
+        int result=TicTacToe.rowsln(b);
+        assertEquals(exp_res,result,"Row length should is not 5");
+
+    }
+    @Test
+    void row_length_t4()
+    {
+        int [][]b=new int[][]{{O,O,X},{X,X,O},{O,O,X},{O,X,O}};
+        int exp_res=4;
+        int result=TicTacToe.rowsln(b);
+        assertEquals(exp_res,result,"Row length should is not 4");
+
+    }
+    @Test
+    void row_length_t5()
+    {
+        int [][]b=new int[][]{{X,X,O,O,X},{X,O,X,X,O},{O,O,X,O,O}};
+        int exp_res=3;
+        int result=TicTacToe.rowsln(b);
+        assertEquals(exp_res,result,"Row length should is not 3");
+
+    }
 }
