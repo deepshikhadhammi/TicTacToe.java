82b9a15f778ff2c3c446f53112537be514699eca
deepshikha.dhammi <deepshikhadhammi@ucalgary.ca>
2022-02-09 22:43:55 -0700

    Check whether there is a win in  diagonal backwards using different tests.

--- src/TicTacToeTest.java
+++ src/TicTacToeTest.java
@@ -331,5 +331,41 @@ class TicTacToeTest {
         boolean result=TicTacToe.winInColumn(b,c,O);
         assertTrue(result);
     }
+    @Test
+    void winDiagonal_BS_t1()
+    {
+        int[][] b = new int[][]{{X, X, O}, {O, X, O}, {O, O, X}};
+        boolean result = TicTacToe.winInDiagonalBS(b, X);
+        assertTrue(result);
+    }
+    @Test
+    void winDiagonal_BS_t2()
+    {
+        int[][] b = new int[][]{{X,O,X,O},{O,O,X,X},{X,X,O,O},{O,O,X,O}};
+        boolean result = TicTacToe.winInDiagonalBS(b,O);
+        assertTrue(result);
+    }
+    @Test
+    void winDiagonal_BS_t3()
+    {
+        int[][] b = new int[][]{{O,X,O,X,X},{O,X,X,O,O},{X,O,X,O,X},{O,X,O,X,O},{O,X,O,X,O}};
+        boolean result = TicTacToe.winInDiagonalBS(b, X);
+        assertTrue(result);
+    }
+    @Test
+    void winDiagonal_BS_t4()
+    {
+        int[][] b = new int[][]{{O,X,O,O},{X,O,X,X},{X,X,O,O},{O,X,O,X}};
+        boolean result = TicTacToe.winInDiagonalBS(b, O);
+        assertTrue(result);
+    }
+    @Test
+    void winDiagonal_BS_t5()
+    {
+        int[][] b = new int[][]{{O,X,X,O,X},{X,O,O,X,X},{O,X,X,O,O},{X,O,O,X,X},{X,X,O,O,X}};
+        boolean result = TicTacToe.winInDiagonalBS(b, X);
+        assertTrue(result);
+    }
+
 
 }
